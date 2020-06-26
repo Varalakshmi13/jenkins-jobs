9e5447fa413955a2f62b7f132634f8c54496963b
Varalakshmi13 <varalakshmi.ganjam@gmail.com>
2020-06-26 19:11:26 +0200

    changing to see

--- seed.groovy
+++ seed.groovy
@@ -39,3 +39,85 @@ pipelineJob('Code-COmpile') {
   }
   
 }
+
+pipelineJob('Code-qualitycheck') {  
+  configure { flowdefinition ->    
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {      
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {        
+        'configVersion'(2)        
+        'userRemoteConfigs' {          
+          'hudson.plugins.git.UserRemoteConfig' {            
+            'url'('https://github.com/Varalakshmi13/jenkins-jobs.git')
+            
+          }
+          
+        }
+        
+        'branches' {
+          
+          'hudson.plugins.git.BranchSpec' {
+            
+            'name'('*/master')
+            
+          }
+          
+        }
+        
+        'doGenerateSubmoduleConfigurations'(false)
+        
+        'submoduleCfg'(class:'list')
+        
+        'extensions'()
+        
+      }
+      
+      'scriptPath'('quality.Jenkinsfile')
+      
+      'lightweight'(true)
+      
+    }
+    
+  }
+  
+}
+
+pipelineJob('Packaging-upload-artifacts') {  
+  configure { flowdefinition ->    
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {      
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {        
+        'configVersion'(2)        
+        'userRemoteConfigs' {          
+          'hudson.plugins.git.UserRemoteConfig' {            
+            'url'('https://github.com/Varalakshmi13/jenkins-jobs.git')
+            
+          }
+          
+        }
+        
+        'branches' {
+          
+          'hudson.plugins.git.BranchSpec' {
+            
+            'name'('*/master')
+            
+          }
+          
+        }
+        
+        'doGenerateSubmoduleConfigurations'(false)
+        
+        'submoduleCfg'(class:'list')
+        
+        'extensions'()
+        
+      }
+      
+      'scriptPath'('packaging.Jenkinsfile')
+      
+      'lightweight'(true)
+      
+    }
+    
+  }
+  
+}
